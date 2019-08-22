fb812b7bf6c66f5d7d17f0c708b45fa5668a6be1
shintakuix <candeiasva@gmail.com>
2019-08-22 01:56:30 -0400

    heroku

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://heroku_2rqcs8wp:Xiamu123@ds211718.mlab.com:11718/heroku_2rqcs8wp",
+    mongoURI: "mongodb://jose:Xiamu123@ds211718.mlab.com:11718/heroku_2rqcs8wp",
     secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -20,8 +20,8 @@ const db = require("./config/keys").mongoURI;
 //MongoDB Connection
 mongoose
   .connect(
-    db,
-    { useNewUrlParser: true }
+    db/* ,
+    { useNewUrlParser: true } */
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
@@ -43,5 +43,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //Connection
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
