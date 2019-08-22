4e49d42ad7098dff30cc10f228215c62b69a1b3a
shintakuix <candeiasva@gmail.com>
2019-08-22 01:49:25 -0400

    heroku

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://<dbuser>:<dbpassword>@ds211718.mlab.com:11718/heroku_2rqcs8wp",
+    mongoURI: "mongodb://heroku_2rqcs8wp:Xiamu123@ds211718.mlab.com:11718/heroku_2rqcs8wp",
     secretOrKey: "secret"
 };
